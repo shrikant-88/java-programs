18833ea676d84d122bb48f541b3e307c5bb81d1a
shrikant-88 <137672925+shrikant-88@users.noreply.github.com>
2023-07-02 12:08:56 +0530

    Create initialize.java

---
+++ initialize.java
@@ -0,0 +1,7 @@
+public class datatype_initialization {
+    public static void main(String[] args) {
+        int n = 23; //initialization of int datatype
+        double pi = 3.14; //initialization of double datatype
+        String name = "XYZ"; //initialization of String datatype
+    }
+}
